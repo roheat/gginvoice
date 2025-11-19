e1cc356d9de075034c10994ed4e6efd20da963e9
Rohith Srivathsav <rohith@multisafe.finance>
2025-11-19 21:00:11 +0400

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ifjpnobeyvvpzvsgspva.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
